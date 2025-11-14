a07a9c3cf93546e9cf1823e620a90b59494bf893
seaG7 <neyro.yt@gmail.com>
2025-11-15 01:12:34 +0300

    chat start

--- chat-bot/bot.ts
+++ chat-bot/bot.ts
@@ -1,5 +1,7 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
 import { Bot, Keyboard, Context } from '@maxhub/max-bot-api';
+
+dotenv.config();
 
 const { BOT_TOKEN, BOT_NAME } = process.env;
 
@@ -11,16 +13,7 @@ const MINI_APP_URL = `https://max.ru/${BOT_NAME}?startapp`;
 
 const bot = new Bot(BOT_TOKEN);
 
-bot.api
-  .setMyCommands([
-    {
-      name: 'start',
-      description: 'Запустить приложение',
-    },
-  ])
-  .catch((err) => console.error('Failed to set commands:', err));
-
-bot.command('start', (ctx: Context) => {
+const sendWelcomeMessage = (ctx: Context) => {
   const text =
     'Добро пожаловать в «МАХ Добро»! ✨\n\nЭто платформа, которая поможет вам найти возможности для добрых дел, отслеживать свой вклад и развивать социальные компетенции.';
 
@@ -31,6 +24,19 @@ bot.command('start', (ctx: Context) => {
   return ctx.reply(text, {
     attachments: [keyboard],
   });
-});
+};
+
+bot.api
+  .setMyCommands([
+    {
+      name: 'start',
+      description: 'Запустить приложение',
+    },
+  ])
+  .catch((err) => console.error('Failed to set commands:', err));
+
+bot.command('start', sendWelcomeMessage);
+
+bot.on('bot_started', sendWelcomeMessage);
 
 bot.start();
