3882d61ef4ebe8264731b9c9262fda8dc8bc5f40
krl76 <k.kornilov2005@yandex.ru>
2025-11-15 02:44:12 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     origin: [
       'https://max-dobro.vercel.app',
       'http://localhost:3000',
-      'http://localhost:5173',
+      'https://mvqcvuhuhngolsewmlob.supabase.co',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
