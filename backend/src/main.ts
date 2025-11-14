83cbd338de73f30a3bb81bd136ee90bcfade1338
krl76 <k.kornilov2005@yandex.ru>
2025-11-15 01:15:55 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'https://max-dobro.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
