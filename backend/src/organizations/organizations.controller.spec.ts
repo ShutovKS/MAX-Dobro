328d95315a94b501b032de40e32da19c43de7799
krl76 <k.kornilov2005@yandex.ru>
2026-05-10 18:43:29 +0300

    test: fix organization specs

--- backend/src/organizations/organizations.controller.spec.ts
+++ backend/src/organizations/organizations.controller.spec.ts
@@ -1,5 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { OptionalAuthGuard } from '../auth/guards/optional-auth.guard';
+import { EventsService } from '../events/events.service';
+import { ReviewsService } from '../reviews/reviews.service';
 import { OrganizationsController } from './organizations.controller';
+import { OrganizationsService } from './organizations.service';
+
+const mockGuard = { canActivate: jest.fn(() => true) };
 
 describe('OrganizationsController', () => {
   let controller: OrganizationsController;
@@ -7,7 +14,17 @@ describe('OrganizationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationsController],
-    }).compile();
+      providers: [
+        { provide: OrganizationsService, useValue: {} },
+        { provide: ReviewsService, useValue: {} },
+        { provide: EventsService, useValue: {} },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(mockGuard)
+      .overrideGuard(OptionalAuthGuard)
+      .useValue(mockGuard)
+      .compile();
 
     controller = module.get<OrganizationsController>(OrganizationsController);
   });
